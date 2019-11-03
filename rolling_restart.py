b9ca301fc6a40c46394ac3534c625a6a2fc44381
Luca Pasquali <lpasqua@gmail.com>
2019-11-03 20:15:54 +1100

    fix indexes reference

--- rolling_restart.py
+++ rolling_restart.py
@@ -2,30 +2,32 @@ import boto3
 import sys
 import time
 
-kinesis_client = boto3.client('autoscaling')
+asg_client = boto3.client('autoscaling')
 autoscaling_group_name = sys.argv[1]
 
 # describing the autoscaling group obtained in input
-auto_scaling_group_response = client.describe_auto_scaling_groups(
+auto_scaling_group_response = asg_client.describe_auto_scaling_groups(
     AutoScalingGroupNames=[
         autoscaling_group_name,
     ],
 )
+count = -1
 
 # iterate for each instance in the autoscaling group
-for i in auto_scaling_group_response['AutoScalingGroups']['Instances']:
-    printf("Destroying instance %s\n", auto_scaling_group_response['AutoScalingGroups']['Instances'][i]['InstanceId'])
+for i in auto_scaling_group_response['AutoScalingGroups'][0]['Instances']:
+    count = count +1
+    print("Destroying instance %s\n" % i['InstanceId'])
 
     # destroying instance in autoscaling group
     # the destruction forces the replacement of the instance
-    scaling_activity_response = client.terminate_instance_in_auto_scaling_group(
-        InstanceId=auto_scaling_group_response['AutoScalingGroups']['Instances'][i]['InstanceId'],
+    scaling_activity_response = asg_client.terminate_instance_in_auto_scaling_group(
+        InstanceId=auto_scaling_group_response['AutoScalingGroups'][0]['Instances'][count]['InstanceId'],
         ShouldDecrementDesiredCapacity=False
     )
 
     # Checking the status of the auto scaling activity
-    while true:
-        scaling_activity_status_response = client.describe_scaling_activities(
+    while True:
+        scaling_activity_status_response = asg_client.describe_scaling_activities(
             ActivityIds=[
                 scaling_activity_response['Activity']['ActivityId'],
             ],
@@ -38,5 +40,5 @@ for i in auto_scaling_group_response['AutoScalingGroups']['Instances']:
             print("Creation of Instance failed, exiting\n")
             sys.exit(1)
         else:
-            print("Creation of instance in %s status, waiting 10 seconds\n", scaling_activity_status_response['Activities'][0]["StatusCode"])
+            print("Creation of instance in %s status, waiting 10 seconds" % scaling_activity_status_response['Activities'][0]["StatusCode"])
             time.sleep(10)
